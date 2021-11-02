8cef1c0fa69a5a7f077484d5a547b8c6d3180df1
Youssef Girgeis <youssefgirgeis@gmail.com>
2021-11-01 23:16:02 -0400

    display all students on the page

--- js/script.js
+++ js/script.js
@@ -9,8 +9,10 @@ For assistance:
    Reach out in your Slack community: https://treehouse-fsjs-102.slack.com/app_redirect?channel=unit-2
 */
 
+const header = document.querySelector(".header");
+const studentList = document.querySelector(".student-list");
+
 const createSearchBar = () => {
-  const header = document.querySelector(".header");
   const label = document.createElement("label");
   const searchByName = document.createElement("span");
   const nameInput = document.createElement("input");
@@ -37,6 +39,38 @@ Create the `showPage` function
 This function will create and insert/append the elements needed to display a "page" of nine students
 */
 
+const showPage = (students) => {
+  for (const student of students) {
+    const li = document.createElement("li");
+    const studentDetailsDiv = document.createElement("div");
+    const joinedDetailsDiv = document.createElement("div");
+    const image = document.createElement("img");
+    const h3 = document.createElement("h3");
+    const email = document.createElement("span");
+    const joinedDate = document.createElement("span");
+
+    li.classList.add("student-item", "cf");
+    studentDetailsDiv.classList.add("student-details");
+    joinedDetailsDiv.classList.add("joined-details");
+    image.setAttribute("alt", "Profile Picture");
+    image.classList.add("avatar");
+    email.classList.add("email");
+    joinedDate.classList.add("date");
+
+    image.src = `${student.picture.large}`;
+    studentDetailsDiv.appendChild(image);
+    h3.textContent = `${student.name.first} ${student.name.last}`;
+    studentDetailsDiv.appendChild(h3);
+    email.textContent = `${student.email}`;
+    joinedDate.textContent = `Joined ${student.registered.date}`;
+    joinedDetailsDiv.appendChild(joinedDate);
+    studentDetailsDiv.appendChild(email);
+    li.appendChild(studentDetailsDiv);
+    li.appendChild(joinedDetailsDiv);
+    studentList.appendChild(li);
+  }
+};
+
 /*
 Create the `addPagination` function
 This function will create and insert/append the elements needed for the pagination buttons
@@ -44,3 +78,4 @@ This function will create and insert/append the elements needed for the paginati
 
 // Call functions
 createSearchBar();
+showPage(data);
