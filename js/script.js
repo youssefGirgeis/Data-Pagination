fd62d986f9ae461c0fb91198f616cb8ad87c53a8
Youssef Girgeis <youssefgirgeis@gmail.com>
2021-11-01 21:48:11 -0400

    Refactoring

--- js/script.js
+++ js/script.js
@@ -8,26 +8,29 @@ For assistance:
    Check out the "Project Resources" section of the Instructions tab: https://teamtreehouse.com/projects/data-pagination-and-filtering#instructions
    Reach out in your Slack community: https://treehouse-fsjs-102.slack.com/app_redirect?channel=unit-2
 */
-const header = document.querySelector(".header");
-const label = document.createElement("label");
-const searchByName = document.createElement("span");
-const nameInput = document.createElement("input");
-const searchIcon = document.createElement("img");
-const searchButton = document.createElement("button");
 
-searchButton.type = "button";
-searchIcon.src = "img/icn-search.svg";
-searchIcon.alt = "Search icon";
-searchButton.appendChild(searchIcon);
+const createSearchBar = () => {
+  const header = document.querySelector(".header");
+  const label = document.createElement("label");
+  const searchByName = document.createElement("span");
+  const nameInput = document.createElement("input");
+  const searchIcon = document.createElement("img");
+  const searchButton = document.createElement("button");
 
-label.classList.add("student-search");
-searchByName.textContent = "Search by name";
-nameInput.setAttribute("id", "search");
-nameInput.setAttribute("placeholder", "search by name...");
-label.appendChild(searchByName);
-label.appendChild(nameInput);
-label.appendChild(searchButton);
-header.appendChild(label);
+  searchButton.type = "button";
+  searchIcon.src = "img/icn-search.svg";
+  searchIcon.alt = "Search icon";
+  searchButton.appendChild(searchIcon);
+
+  label.classList.add("student-search");
+  searchByName.textContent = "Search by name";
+  nameInput.setAttribute("id", "search");
+  nameInput.setAttribute("placeholder", "search by name...");
+  label.appendChild(searchByName);
+  label.appendChild(nameInput);
+  label.appendChild(searchButton);
+  header.appendChild(label);
+};
 
 /*
 Create the `showPage` function
@@ -40,3 +43,4 @@ This function will create and insert/append the elements needed for the paginati
 */
 
 // Call functions
+createSearchBar();
